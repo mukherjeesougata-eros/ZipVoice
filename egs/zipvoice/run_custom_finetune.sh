8d87f8ea2a24b865bbcb5df5888702694e241d3c
SOUGATA MUKHERJEE <95704630+mukherjeesougata-eros@users.noreply.github.com>
2026-07-01 16:38:57 +0530

    Change HF_TOKEN placeholder in run_custom_finetune.sh
    Updated the placeholder for the HF_TOKEN in the script.

--- egs/zipvoice/run_custom_finetune.sh
+++ egs/zipvoice/run_custom_finetune.sh
@@ -118,7 +118,7 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
       echo "Stage 4: Download pre-trained model, tokens file, and model config"
       # Uncomment this line to use HF mirror
       #export HF_ENDPOINT=https://hf-mirror.com
-      #export HF_TOKEN=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
+      #export HF_TOKEN=hf_your_token
       hf_repo=k2-fsa/ZipVoice
       mkdir -p ${download_dir}
       for file in model.pt tokens.txt model.json; do
